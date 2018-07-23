27bcd32541c25fd7b45245514d07337280c57d69
Khin Myint Myat Thu <kmmt96@gmail.com>
2018-07-24 02:20:26 +0630

    Implement basic login

--- Gemfile
+++ Gemfile
@@ -5,11 +5,11 @@ ruby '2.3.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
-gem 'bcrypt',  '3.1.12'
 # Use sqlite3 as the database for Active Record
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 gem 'sqlite3','1.3.11'
+gem 'jquery-rails'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   get 'users/new'
   get '/help', to:'static_pages#help'
   get '/about', to:'static_pages#about'
@@ -8,5 +9,8 @@ Rails.application.routes.draw do
   post '/signup', to:'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources:users
 end
