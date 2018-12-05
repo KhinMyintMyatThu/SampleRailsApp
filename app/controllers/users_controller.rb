ab22c11d2e76b71459b39d55b18f43780d6d7ead
Khin Myint Myat Thu <kmmt96@gmail.com>
2018-12-05 12:34:02 +0630

    Sample App

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,16 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
-  def show
-   @user = User.find(params[:id])
-   #debugger
-  end
-
   def index
+    #@users = User.where(activated: FILL_IN).paginate(page: params[:page])
     @users = User.paginate(page: params[:page])
   end
+
+   def show
+     @user = User.find(params[:id])
+    #redirect_to root_url
+    #and return unless FILL_IN
+   end
 
  def new
    @user=User.new
--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2018_12_04_083735) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "activation_digest"
-    t.boolean "activated"
+    t.boolean "activated", default: false
     t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
